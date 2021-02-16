231102bb1d57f016d436b05cae56bfacb9ca753e
Snegovsky <nik.snegovskyvlad@gmail.com>
2021-02-16 22:55:43 +0200

    test

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ async def scheduled(message):
     while True:
         await asyncio.sleep(20)
         now = datetime.datetime.now()
+        print("Here2 ==========")
         if now.minute == 0:
             print("Ask ==========")
             locations = db.get_all_locations(message.from_user.id)
